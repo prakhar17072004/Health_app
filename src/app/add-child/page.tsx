203e921385c73d02b278e8d2a07c5ed3653f1bd7
prakhar17072004 <pmankur2004@gmail.com>
2025-11-15 22:41:09 +0530

    css update

--- src/app/add-child/page.tsx
+++ src/app/add-child/page.tsx
@@ -39,7 +39,7 @@ export default function AddChildPage() {
 
   return (
     <div className="max-w-md mx-auto mt-9 p-8 border rounded shadow">
-      <h2 className="text-2xl font-semibold mb-8">Add Child</h2>
+      <h2 className="text-2xl font-semibold mb-9">Add Child</h2>
 
       <label className="block mb-6 font-medium">Parent Name</label>
       <input
